afe905c6c6d319ad9f6ede06e7d2940a147a8ba7
Claire-Aline Haestie <ca.haestie@gmail.com>
2026-01-05 11:49:58 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
-import Routes from './Routes'
-import React, {useEffect} from 'react'
-
+import "./App.css";
+import Routes from "./Routes";
+import { useEffect } from "react";
+import AOS from "aos";
 function App(props) {
-  
+  useEffect(() => {
+    const initAOS = () => {
+      AOS.init({
+        disable: "mobile",
+      });
+    };
+
+    initAOS();
+
+    const handleResize = () => {
+      AOS.refresh();
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div>
       <Routes hideLoader={props.hideLoader} showLoader={props.showLoader} />
